7b23414e3de2eaab8598cf826103129a8d5e57b4
Nahuel Clotet <nahuee2011@gmail.com>
2022-12-01 11:42:58 -0300

    Add x3 and x1 to the leaderboard

--- models/Leaderboard.js
+++ models/Leaderboard.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const leaderboardSchema = new mongoose.Schema({
     uid: String,
     displayName: String,
-    points: Number
+    points: Number,
+    x3: Number,
+    x1: Number
 });
 
 export const RowLeaderboard = mongoose.model('Leaderboard', leaderboardSchema);
--- routes/leaderboardRouter.js
+++ routes/leaderboardRouter.js
@@ -19,18 +19,28 @@ leaderboardRouter.get('/update', async (req, res) => {
 
     users.map(async (user) => {
         let points = 0;
+        let x1 = 0;
+        let x3 = 0;
         user.results.forEach(res => {
 
             const match = matches.find(m => m.MatchNumber === res.MatchNumber);
             if (!match) return;
             const matchResult = getResultPrediction(match);
-            if (match.HomeTeamScore === res.HomeTeamScore && match.AwayTeamScore === res.AwayTeamScore) points += 3;
-            else if (matchResult === res.result) points += 1;
+            if (match.HomeTeamScore === res.HomeTeamScore && match.AwayTeamScore === res.AwayTeamScore) {
+                points += 3;
+                x3++;
+            }
+            else if (matchResult === res.result) {
+                points += 1;
+                x1++;
+            }
         });
         await RowLeaderboard.findOneAndUpdate({ uid: user.uid }, {
             displayName: user.displayName,
             uid: user.uid,
-            points
+            points,
+            x1,
+            x3
         }, { upsert: true });
 
     });
